d89b372d16e7542080eefc57d5ca90f409cb8d1b
chinajianshen <281349838@qq.com>
2020-04-30 20:21:40 +0800

    修改配置文件11

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 image_version=`date +%Y%m%d%H%M`;
 echo $image_version;
-# cd Jenkins2Docker
+cd Jenkins2Docker
 git pull --rebase origin master;
 docker stop jenkins2docker;
 docker rm jenkins2docker;
-docker build -f ./Jenkins2Docker/Dockerfile -t jenkins2docker:$image_version .;
+docker build -f Dockerfile -t jenkins2docker:$image_version .;
 docker images;
-docker run -p 10001:80 -d --name jenkins2docker jenkins2docker:$image_version;
-# -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
+docker run -p 10001:80 -d --restart=always --name jenkins2docker jenkins2docker:$image_version ;
+# -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   #--restart=always
 docker logs jenkins2docker;
 #删除build过程中产生的镜像    #docker image prune -a -f
 docker rmi $(docker images -f "dangling=true" -q)
